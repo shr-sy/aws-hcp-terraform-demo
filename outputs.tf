6876bd9c88dc52802bb5f7f3ac8093661aec8d1e
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-10-14 12:22:44 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,5 @@
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "ec2_public_ip" {
-  value = aws_instance.web.public_ip
-}
-
-output "website_url" {
-  description = "Public web server URL"
-  value       = "http://${aws_instance.web.public_ip}"
+output "ec2_private_ip" {
+  value = aws_instance.web.private_ip
 }
 
 output "s3_bucket_name" {

76f7c2a702d4d3993af9f9db12617d65a8654722
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-10-14 15:35:36 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+output "alb_url" {
+  value       = aws_lb.web_alb.dns_name
+  description = "Public URL to access the website via ALB"
+}
+
 output "ec2_private_ip" {
   value = aws_instance.web.private_ip
 }
@@ -5,9 +10,3 @@ output "ec2_private_ip" {
 output "s3_bucket_name" {
   value = aws_s3_bucket.app_data.bucket
 }
-
-output "alb_url" {
-  value       = aws_lb.web_alb.dns_name
-  description = "Public URL to access the website via ALB"
-}
-

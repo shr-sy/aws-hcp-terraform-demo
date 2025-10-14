1f51de447f847e53c63e878113890fc9e5c87995
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-10-14 15:29:01 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,23 +1,27 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-  tags = { Name = "${var.project_name}-vpc" }
+  tags       = { Name = "${var.project_name}-vpc" }
 }
 
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  tags = { Name = "${var.project_name}-public-subnet" }
+  tags                    = { Name = "${var.project_name}-public-subnet" }
 }
 
 resource "aws_subnet" "private" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = false
-  tags = { Name = "${var.project_name}-private-subnet" }
+  tags                    = { Name = "${var.project_name}-private-subnet" }
 }
 
-# Public Route Table
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags   = { Name = "${var.project_name}-igw" }
+}
+
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
@@ -27,11 +31,6 @@ resource "aws_route_table" "public" {
   }
 
   tags = { Name = "${var.project_name}-public-rt" }
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-  tags   = { Name = "${var.project_name}-igw" }
 }
 
 resource "aws_route_table_association" "public_assoc" {

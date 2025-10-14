1dda85456e5472a3b5c60a2dd961bf34cf75a4e5
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-10-14 15:48:35 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,21 +1,52 @@
+##########################
+# VPC
+##########################
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   tags       = { Name = "${var.project_name}-vpc" }
 }
 
-resource "aws_subnet" "public" {
+##########################
+# Public Subnets (for ALB / Web)
+##########################
+
+# Public Subnet 1 (AZ: ap-south-1a)
+resource "aws_subnet" "public_a" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
-  tags                    = { Name = "${var.project_name}-public-subnet" }
+
+  tags = { Name = "${var.project_name}-public-a" }
 }
+
+# Public Subnet 2 (AZ: ap-south-1b)
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = true
+
+  tags = { Name = "${var.project_name}-public-b" }
+}
+
+##########################
+# Private Subnet (for EC2 / App Layer)
+##########################
 
 resource "aws_subnet" "private" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = false
-  tags                    = { Name = "${var.project_name}-private-subnet" }
+
+  tags = { Name = "${var.project_name}-private" }
 }
+
+##########################
+# Internet Gateway + Routes
+##########################
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
@@ -33,7 +64,16 @@ resource "aws_route_table" "public" {
   tags = { Name = "${var.project_name}-public-rt" }
 }
 
-resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = aws_subnet.public.id
+##########################
+# Route Table Associations
+##########################
+
+resource "aws_route_table_association" "public_a_assoc" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_b_assoc" {
+  subnet_id      = aws_subnet.public_b.id
   route_table_id = aws_route_table.public.id
 }

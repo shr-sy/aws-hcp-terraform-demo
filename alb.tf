c9e4840002ae3c39cd727f39e75b115b166ae273
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-10-14 16:52:48 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -1,45 +1,13 @@
-##########################
-# Security Group for ALB
-##########################
-resource "aws_security_group" "alb_sg" {
-  name        = "${var.project_name}-alb-sg"
-  description = "Allow HTTP traffic to ALB"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = { Name = "${var.project_name}-alb-sg" }
-}
-
-##########################
-# Application Load Balancer
-##########################
 resource "aws_lb" "web_alb" {
   name               = "${var.project_name}-alb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
-  # Use both public subnets in different AZs
   subnets            = [aws_subnet.public_a.id, aws_subnet.public_b.id]
 
   tags = { Name = "${var.project_name}-alb" }
 }
 
-##########################
-# Target Group for EC2
-##########################
 resource "aws_lb_target_group" "web_tg" {
   name        = "${var.project_name}-tg"
   port        = 80
@@ -59,18 +27,12 @@ resource "aws_lb_target_group" "web_tg" {
   tags = { Name = "${var.project_name}-tg" }
 }
 
-##########################
-# Attach EC2 to Target Group
-##########################
 resource "aws_lb_target_group_attachment" "web_attachment" {
   target_group_arn = aws_lb_target_group.web_tg.arn
   target_id        = aws_instance.web.id
   port             = 80
 }
 
-##########################
-# Listener for ALB
-##########################
 resource "aws_lb_listener" "web_listener" {
   load_balancer_arn = aws_lb.web_alb.arn
   port              = 80
@@ -82,9 +44,6 @@ resource "aws_lb_listener" "web_listener" {
   }
 }
 
-##########################
-# Output ALB DNS Name
-##########################
 output "alb_url" {
   value       = aws_lb.web_alb.dns_name
   description = "Public URL to access the website via ALB"

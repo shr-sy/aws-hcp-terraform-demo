ab4f5b4a6990279df59e302a1ef95f0edbcad287
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-10-14 12:21:46 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,34 +1,21 @@
-# Generate a random suffix for bucket name to ensure uniqueness
-resource "random_id" "suffix" {
-  byte_length = 4
-}
+resource "random_id" "suffix" { byte_length = 4 }
 
-# S3 Bucket
 resource "aws_s3_bucket" "app_data" {
   bucket = "${var.project_name}-data-${random_id.suffix.hex}"
-
-  tags = {
-    Name = "${var.project_name}-bucket"
-  }
+  tags   = { Name = "${var.project_name}-bucket" }
 }
 
-# Block public access for security
 resource "aws_s3_bucket_public_access_block" "block" {
-  bucket = aws_s3_bucket.app_data.id
-
+  bucket                  = aws_s3_bucket.app_data.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
-# Upload a demo HTML file using aws_s3_object
 resource "aws_s3_object" "demo_file" {
-  bucket  = aws_s3_bucket.app_data.id
-  key     = "index.html"
-  content = <<EOF
-<h1>Hello from Terraform on AWS!</h1>
-<p>This file is served from S3 bucket: ${aws_s3_bucket.app_data.bucket}</p>
-EOF
+  bucket       = aws_s3_bucket.app_data.id
+  key          = "index.html"
+  content      = "<h1>Hello from Terraform on AWS!</h1><p>Served from S3 bucket: ${aws_s3_bucket.app_data.bucket}</p>"
   content_type = "text/html"
 }

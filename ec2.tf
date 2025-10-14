93b362a1de824ec812a938cccf8186754076f35b
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-10-14 12:21:00 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,36 +1,3 @@
-# Security Group
-resource "aws_security_group" "web_sg" {
-  name        = "${var.project_name}-sg"
-  description = "Allow SSH and HTTP"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.project_name}-sg"
-  }
-}
-
-# Latest Ubuntu AMI
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["099720109477"]
@@ -41,13 +8,12 @@ data "aws_ami" "ubuntu" {
   }
 }
 
-# EC2 instance with user_data to pull HTML from S3
 resource "aws_instance" "web" {
-  ami                         = data.aws_ami.ubuntu.id
-  instance_type               = var.instance_type
-  subnet_id                   = aws_subnet.public.id
-  vpc_security_group_ids      = [aws_security_group.web_sg.id]
-  associate_public_ip_address = true
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.private.id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  associate_public_ip_address = false
 
   user_data = <<-EOF
               #!/bin/bash
@@ -55,12 +21,8 @@ resource "aws_instance" "web" {
               apt install -y apache2 awscli
               systemctl enable apache2
               systemctl start apache2
-              
-              # Download the HTML file from S3
               aws s3 cp s3://${aws_s3_bucket.app_data.bucket}/index.html /var/www/html/index.html
               EOF
 
-  tags = {
-    Name = "${var.project_name}-ec2"
-  }
+  tags = { Name = "${var.project_name}-ec2" }
 }

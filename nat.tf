ea65b320d1e30dc1b3b82b4b17f4d8e160d0f1db
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-10-14 12:08:57 +0530

    Create nat.tf

---
+++ nat.tf
@@ -0,0 +1,26 @@
+resource "aws_eip" "nat_eip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public.id
+  tags = { Name = "${var.project_name}-nat-gateway" }
+}
+
+# Private Route Table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = { Name = "${var.project_name}-private-rt" }
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}

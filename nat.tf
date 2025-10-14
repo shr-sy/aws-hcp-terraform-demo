cca946fe05122961f72e630ba3863e9b4c86544b
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-10-14 15:54:01 +0530

    Update nat.tf

--- nat.tf
+++ nat.tf
@@ -1,16 +1,30 @@
+##########################
+# Elastic IP for NAT Gateway
+##########################
 resource "aws_eip" "nat_eip" {
   domain = "vpc"
 
-  tags = { Name = "${var.project_name}-nat-eip" }
+  tags = {
+    Name = "${var.project_name}-nat-eip"
+  }
 }
 
+##########################
+# NAT Gateway (in Public Subnet A)
+##########################
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public.id
+  subnet_id     = aws_subnet.public_a.id  # ✅ updated
+  tags = {
+    Name = "${var.project_name}-nat"
+  }
 
-  tags = { Name = "${var.project_name}-nat-gateway" }
+  depends_on = [aws_internet_gateway.igw]
 }
 
+##########################
+# Private Route Table
+##########################
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
@@ -19,9 +33,14 @@ resource "aws_route_table" "private" {
     nat_gateway_id = aws_nat_gateway.nat.id
   }
 
-  tags = { Name = "${var.project_name}-private-rt" }
+  tags = {
+    Name = "${var.project_name}-private-rt"
+  }
 }
 
+##########################
+# Private Route Table Association
+##########################
 resource "aws_route_table_association" "private_assoc" {
   subnet_id      = aws_subnet.private.id
   route_table_id = aws_route_table.private.id
